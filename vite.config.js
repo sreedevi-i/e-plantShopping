9359953cac9f3210be86803a49a3f2ca39aacda6
Sreedevi Itta <72245796+sreedevi-i@users.noreply.github.com>
2025-04-14 19:54:50 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  base: "e-shoppingcart",
+  base: "/e-shoppingcart",
   plugins: [react()],
 })
